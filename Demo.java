806ab29d7e6373c94aec82981a0bb7eed89dc3d8
jeromewinifred <jerome.winifred@outlook.com>
2020-08-07 13:00:24 +0000

    Added Java File

---
+++ Demo.java
@@ -0,0 +1,10 @@
+/* This is a simple Java program. 
+   FileName : "Demo.java". */
+class HelloWorld 
+{ 
+    public static void main(String args[]) 
+    { 
+        System.out.println("Simple Demo"); 
+    } 
+} 
+
